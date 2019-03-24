d4d05f7ff909e8bc9cd02b3308d2b87075bb8d70
lozita <alina-lozyuk@mail.ru>
2019-03-24 18:28:13 +0300

    Revert "fix errors"

--- back/lib/mongoose.js
+++ back/lib/mongoose.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/pixelplex_task', function (err) {
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.NAME_DB}`, function (err) {
   if (err) throw err;
   console.log('connection to mongodb is installed.');
 });
